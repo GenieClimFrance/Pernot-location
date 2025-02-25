22f548bb1c8753c62e92c53470f9392602c139fe
Axel <axeljpro@gmail.com>
2025-02-25 09:50:52 +0100

    fix next.config .ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next/types";
 
 const nextConfig: NextConfig = {
   output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
   },
 };
 
